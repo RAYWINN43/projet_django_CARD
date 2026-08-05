f395393c84522d848f25746694df7e5f62573ea9
Cawren <pecontalcorentin@gmail.com>
2026-08-05 09:50:21 +0200

    UPDATE : Dossier game

--- backend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-import views
-
-urlpatterns = [
-    path("game/<int:game_id>/play", views.play_game, name="play_game"),
-    path("game/<int:game_id>/play/<str:move>", views.play_turn, name="play_turn"),
-]
